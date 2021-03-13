f6a72ff3d35f6886aa870845b73f98121ff43214
Kendrick Conston <kconston@gmail.com>
2021-03-13 15:16:06 -0600

    add additional git config to setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
+
+while getopts e:f: flag
+do
+    case "${flag}" in
+        e) email=${OPTARG};;
+        f) fullname=${OPTARG};;
+    esac
+done
+
+#Install git
+sudo apt install git
+git config --global user.email "$email"
+git config --global user.name "$fullname" 
 #Install zsh
 sudo apt install zsh
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-#Install git
 #Install cmake
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 
