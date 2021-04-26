5ea2c7d2d76c9019ec5ebd07cce16c3af283827b
Kendrick Conston <kconston@gmail.com>
2021-04-26 16:20:26 -0500

    forgot bind.lua

---
+++ lua/keymap/bind.lua
@@ -0,0 +1,46 @@
+local function normal_remap()
+  local map = vim.api.nvim_set_keymap
+  local options = { noremap = false }
+
+  map('n', '<leader>rn', '<Plug>(coc-rename)', options)
+end
+
+local function normal_noremap()
+  local map = vim.api.nvim_set_keymap
+  local options = { noremap = true }
+  map('n', '<leader>h', ':wincmd h<CR>', options)
+  map('n', '<leader>j', ':wincmd j<CR>', options)
+  map('n', '<leader>k', ':wincmd k<CR>', options)
+  map('n', '<leader>l', ':wincmd l<CR>', options)
+  map('n', '<leader>nhl', ':nohl<CR>', options)
+  map('n', '<leader>qns', ':qa!<CR>', options)
+  map('n', '<leader>lll', '<C-w>v<CR>', options)
+
+  --Quick Reload
+  map('n', '<leader>rl', ':source %<CR>', options)
+
+  --Win Split
+  map('n', '<leader>vs', ':vsplit<CR> :wincmd l<CR>', options)
+  map('n', '<leader>hs', ':split<CR> :wincmd j<CR>', options)
+
+  --Buffer Line
+  map('n', '<silent>[b', ':BufferLineCycleNext<CR>', options)
+  map('n', '<silent>b]', ':BufferLineCyclePrev<CR>', options)
+end
+
+local function insert_noremap()
+  local map = vim.api.nvim_set_keymap
+  local options = { noremap = true }
+  map('i', 'jk', '<esc>', options)
+end
+
+local function other_map()
+  local map = vim.api.nvim_set_keymap
+end
+
+function Map()
+  normal_remap()
+  normal_noremap()
+  insert_noremap()
+  other_map()
+end
